723302d2e3718a075e812748aba24b1dbdfd46f1
Herman Junge <chpdg42@gmail.com>
2017-09-24 03:43:13 -0300

    Add a reason

--- docker/compiler-get-all-deps.sh
+++ docker/compiler-get-all-deps.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+################################################################################
+#
+# The reason we are doing this here and not in the Dockerfile is that
+# you may want to update your packages without wanting to build the image
+# everytime.
+#
+################################################################################
+
 go get -v github.com/ipfs/go-ipfs
 cd /go/src/github.com/ipfs/go-ipfs/
 make deps
